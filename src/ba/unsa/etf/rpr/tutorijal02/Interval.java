80ab4c463fc85a193b5a34dfed3db9ae28cf67a2
amirnakic <anakic1@etf.unsa.ba>
2018-10-23 17:49:33 +0200

    Dodavanje setera klasi Interval.java.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -3,7 +3,11 @@ package ba.unsa.etf.rpr.tutorijal02;
 public class Interval {
     private double pocetnaTacka, krajnjaTacka;
     private boolean pripadnostPocetneTacke, pripadnostKrajnjeTacke;
-    public Interval() { this(0,0,false,false); }
+
+    public Interval() {
+        this(0, 0, false, false);
+    }
+
     public Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadnostPocetneTacke, boolean pripadnostKrajnjeTacke) throws IllegalArgumentException {
         if (pocetnaTacka > krajnjaTacka) throw new IllegalArgumentException("Neispravni parametri!");
         this.pocetnaTacka = pocetnaTacka;
@@ -11,23 +15,45 @@ public class Interval {
         this.pripadnostPocetneTacke = pripadnostPocetneTacke;
         this.pripadnostKrajnjeTacke = pripadnostKrajnjeTacke;
     }
+
     public double getPocetnaTacka() {
         return pocetnaTacka;
     }
+
     public double getKrajnjaTacka() {
         return krajnjaTacka;
     }
+
     public boolean isPripadnostPocetneTacke() {
         return pripadnostPocetneTacke;
     }
+
     public boolean isPripadnostKrajnjeTacke() {
         return pripadnostKrajnjeTacke;
     }
+
+    public void setPocetnaTacka(double pocetnaTacka) {
+        this.pocetnaTacka = pocetnaTacka;
+    }
+
+    public void setKrajnjaTacka(double krajnjaTacka) {
+        this.krajnjaTacka = krajnjaTacka;
+    }
+
+    public void setPripadnostPocetneTacke(boolean pripadnostPocetneTacke) {
+        this.pripadnostPocetneTacke = pripadnostPocetneTacke;
+    }
+
+    public void setPripadnostKrajnjeTacke(boolean pripadnostKrajnjeTacke) {
+        this.pripadnostKrajnjeTacke = pripadnostKrajnjeTacke;
+    }
+
     public boolean isNull() {
-        if(this.getKrajnjaTacka()==0 && this.getPocetnaTacka()==0 && !pripadnostPocetneTacke && !pripadnostKrajnjeTacke)
+        if (this.getKrajnjaTacka() == 0 && this.getPocetnaTacka() == 0 && !pripadnostPocetneTacke && !pripadnostKrajnjeTacke)
             return true;
         return false;
     }
+
     public boolean isIn(double vrijednostTacke) {
         if (this.getPocetnaTacka() < vrijednostTacke && vrijednostTacke < this.getKrajnjaTacka())
             return true;
