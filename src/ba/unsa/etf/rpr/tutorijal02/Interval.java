21bb88622352a0097672a1757a21412a12ab67ba
amirnakic <anakic1@etf.unsa.ba>
2018-10-23 17:37:12 +0200

    Dodavanje konstruktora klasi Interval.java.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,15 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double pocetnaTacka, krajnjaTacka;
+    private boolean pripadnostPocetneTacke, pripadnostKrajnjeTacke;
+    public Interval() { this(0,0,false,false); }
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadnostPocetneTacke, boolean pripadnostKrajnjeTacke) throws IllegalArgumentException {
+        if (pocetnaTacka > krajnjaTacka) throw new IllegalArgumentException("Neispravni parametri!");
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pripadnostPocetneTacke = pripadnostPocetneTacke;
+        this.pripadnostKrajnjeTacke = pripadnostKrajnjeTacke;
+    }
     
-
 }
